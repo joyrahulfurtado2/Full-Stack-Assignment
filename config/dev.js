3d889cb30c17bb14b34b092f0b348aee130c7b5b
joyrahulfurtado2 <jrahul2@outlook.com>
2021-08-03 21:48:58 +0530

    mongo url change

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
     MONGOURI:'mongodb://localhost:27017/ceaTimeLine'
+    // MONGOURI:'mongodb+srv://joy_furtado:joy1234@cluster0.qvhwr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
